80004488447e4b7b3186c6ba07e9ba03a404a416
jeelpatel17 <jeelpateldev@gmail.com>
2021-12-26 17:46:47 +0530

    tried to code palindrome checking program, but was quite unsuccessful

---
+++ FullCWHCourse/checkPalindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[50];
+    int flag;
+    printf("Enter a word to check its palindromity: ");
+    scanf("%s", str);
+    int size = strlen(str);
+    for (int i = 0; i < size; i++)
+    {
+        if (str[i] != str[size - i - 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag)
+    {
+        printf("Pal");
+    }
+    else
+    {
+        printf("Not pal");
+    }
+
+    return 0;
+}
