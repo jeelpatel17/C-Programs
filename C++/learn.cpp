b3222cf0cf244fe2642fb0c642b9f7b4c1c2dc35
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-01 09:50:21 +0530

    started learning c++

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    // DATATYPES IN C++
+    string name = "harry";
+    // int -> short (for short integers)
+    int a = 2;
+    int b = 3;
+    short aVarWithShortMemoryAllocated;
+    float aFloatWhichIsConstant = 45.32;
+    // for declaring constant variables
+    float const aFloatWhichIsConstant = 45.32;
+    // float -> double (for long floats)
+    double score2 = 45.322;
+
+    // for longer integers
+    long aVarWithLongMemoryAllocated;
+    long long aVarWithVeryLongMemoryAllocated;
+    long double score3 = 45.332;
+    cout << "Enter your name:" << endl;
+
+    // Capturing input and storing into a variable
+    cin >> name;
+
+    // Displaying a variable value; similarly for executing functions too instead of variables
+    cout << "Hello " << name << " It's great to meet you!";
+
+    // New line character in C++
+    // endl
+
+    // Typecasting
+    cout << "a / b is " << (float)a / b << endl;
 }
