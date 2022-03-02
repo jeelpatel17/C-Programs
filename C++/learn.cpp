1927e12a3419b2a6adbd8bfcd7e4407427f777e4
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-02 11:12:37 +0530

    learned more about c++ concepts such as recursion, function overloading and static variables

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -2,15 +2,8 @@
 #include <string>
 using namespace std;
 
-union software
-{
-    int id;
-    string name;
-    float pricePerMonth;
-};
-
 int main()
 {
-    union software s1;
-    s1.name = "adobeAe";
+
+    return 1;
 }
