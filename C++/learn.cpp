7b756df133573a699d8f542feb2e00eb24f7538d
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-03 21:53:29 +0530

    winding up for today learning 'protected' in inheritance, enjoying oop so far!

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -2,19 +2,8 @@
 #include <string>
 using namespace std;
 
-class Base
-{
-public:
-    int someVar = 7;
-};
-
-class Derived : public Base
-{
-};
-
 int main()
 {
-    Derived Jeel;
-    cout << Jeel.someVar;
+
     return 1;
 }
