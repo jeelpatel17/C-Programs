a7f92c362de77a474b5c4fad4dc46d4e83fae87e
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-03 21:39:21 +0530

    learned about different types of inheritance in c++

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -2,7 +2,19 @@
 #include <string>
 using namespace std;
 
+class Base
+{
+public:
+    int someVar = 7;
+};
+
+class Derived : public Base
+{
+};
+
 int main()
 {
+    Derived Jeel;
+    cout << Jeel.someVar;
     return 1;
 }
