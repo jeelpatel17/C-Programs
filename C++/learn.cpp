eed25c9a8e8d2a62505b3a8b54ff604bd95dad03
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-05 10:37:39 +0530

    learned about initialization list in inheritance

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -1,60 +1,22 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
-class SimpleCalc
+/* *** Initialization list in constructors *** */
+class some
 {
     int a, b;
 
 public:
-    void takeInSimple()
+    // This is a syntax through which a and b can be given values via constructor
+    some(int c, int d) : a(c), b(d)
     {
-        cout << "Enter value of a:" << endl;
-        cin >> a;
-        cout << "Enter value of b:" << endl;
-        cin >> b;
-        putOutSimple();
+        cout << a << " " << b << endl;
     }
-    void putOutSimple()
-    {
-        cout << a << " + " << b << " = " << a + b << endl;
-        cout << a << " - " << b << " = " << a - b << endl;
-        cout << a << " * " << b << " = " << a * b << endl;
-        cout << a << " / " << b << " = " << a / b << endl;
-    }
-};
-
-class SciCalc
-{
-    int a, b;
-
-public:
-    void takeInSci()
-    {
-        cout << "Enter value of a:" << endl;
-        cin >> a;
-        cout << "Enter value of b:" << endl;
-        cin >> b;
-        putOutSci();
-    }
-    void putOutSci()
-    {
-        cout << "sin of a is " << sin(a) << endl;
-        cout << "cos of a is " << cos(a) << endl;
-        cout << "exp of a is " << exp(a) << endl;
-        cout << "tan of a is " << tan(a) << endl;
-    }
-};
-
-class HybridCalc : public SimpleCalc, public SciCalc
-{
 };
 
 int main()
 {
-    HybridCalc inst;
-    inst.takeInSimple();
-    inst.takeInSci();
+    some thing(1, 2);
     return 1;
 }
