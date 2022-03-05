7211b64f59f94ef7064a535804e4b3820c987c7e
jeelpatel17 <jeelpateldev@gmail.com>
2022-03-05 09:43:47 +0530

    learned more into inheritance

--- C++/learn.cpp
+++ C++/learn.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
+
+class SimpleCalc
+{
+    int a, b;
+
+public:
+    void takeInSimple()
+    {
+        cout << "Enter value of a:" << endl;
+        cin >> a;
+        cout << "Enter value of b:" << endl;
+        cin >> b;
+        putOutSimple();
+    }
+    void putOutSimple()
+    {
+        cout << a << " + " << b << " = " << a + b << endl;
+        cout << a << " - " << b << " = " << a - b << endl;
+        cout << a << " * " << b << " = " << a * b << endl;
+        cout << a << " / " << b << " = " << a / b << endl;
+    }
+};
+
+class SciCalc
+{
+    int a, b;
+
+public:
+    void takeInSci()
+    {
+        cout << "Enter value of a:" << endl;
+        cin >> a;
+        cout << "Enter value of b:" << endl;
+        cin >> b;
+        putOutSci();
+    }
+    void putOutSci()
+    {
+        cout << "sin of a is " << sin(a) << endl;
+        cout << "cos of a is " << cos(a) << endl;
+        cout << "exp of a is " << exp(a) << endl;
+        cout << "tan of a is " << tan(a) << endl;
+    }
+};
+
+class HybridCalc : public SimpleCalc, public SciCalc
+{
+};
 
 int main()
 {
-
+    HybridCalc inst;
+    inst.takeInSimple();
+    inst.takeInSci();
     return 1;
 }
